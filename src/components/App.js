be0524a946bd8fed3a7eba0d42186a2a3ea57543
Charley DAVID <calyhre@gmail.com>
2016-12-18 16:02:15 +0100

    Fix public urls

--- src/components/App.js
+++ src/components/App.js
@@ -36,7 +36,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           {
             this.props.connected ? (
