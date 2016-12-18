673d849ac21596687a62eec7b72fcac7afa06de0
Charley DAVID <calyhre@gmail.com>
2016-12-18 15:54:51 +0100

    Add Hot module reload

--- src/index.js
+++ src/index.js
@@ -5,7 +5,21 @@ import App from 'components/App';
 // eslint-disable-next-line
 import './index.css';
 
+const rootEl = document.getElementById('root');
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  rootEl
 );
+
+if (module.hot) {
+  module.hot.accept('components/App', () => {
+    // eslint-disable-next-line global-require
+    const NextApp = require('components/App').default;
+
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    );
+  });
+}
