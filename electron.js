993141b8a2260be2a19169e72e551029b49f5b9a
agix <florian.gaultier@gmail.com>
2017-01-02 18:18:11 +0100

    Fix bug between Windows and Posix path

--- electron.js
+++ electron.js
@@ -21,7 +21,7 @@ function createWindow() {
       const url = request.url.split('/static/')[1];
       callback({ path: path.join(__dirname, 'build/static', url) });
     } else if (request.url.endsWith('/index.html')) {
-      callback({ path: request.url.substring(prefix.length + 2) });
+      callback({ path: request.url.substring(prefix.length + 3) });
     } else {
       callback({ path: path.join(__dirname, 'build/index.html') });
     }
@@ -33,7 +33,7 @@ function createWindow() {
     height: 800,
   });
   // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/build/index.html`);
+  win.loadURL('file://./build/index.html');
 
   // Open the DevTools.
   // win.webContents.openDevTools();
