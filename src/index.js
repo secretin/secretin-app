f26e13b2a3b4934f905c3e570da28c9faa594bd2
Charley DAVID <hello@calyh.re>
2017-07-26 20:59:20 +0200

    Upgrade react-scripts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
-import registerServiceWorker from 'registerServiceWorker';
+import registerServiceWorker from 'utils/registerServiceWorker';
 
 // eslint-disable-next-line
 import './index.css';
