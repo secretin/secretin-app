6f445e31f922052d131f379c262aa58b98ce47b5
agix <florian.gaultier@gmail.com>
2020-01-15 17:30:33 +0100

    Updates

---
+++ precache-manifest.b8ec4de3c5a258bd6c4f78e6ddfde7ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19300b2968bf1f6a0f4b",
+    "url": "/static/js/main.3e4028cf.chunk.js"
+  },
+  {
+    "revision": "232609452eb404057433",
+    "url": "/static/js/2.37aac862.chunk.js"
+  },
+  {
+    "revision": "19300b2968bf1f6a0f4b",
+    "url": "/static/css/main.bfe4c85b.chunk.css"
+  },
+  {
+    "revision": "51dac36ceab4f8759b7e76700120da72",
+    "url": "/index.html"
+  }
+];
