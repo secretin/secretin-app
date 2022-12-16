49c9c31ddf670b170b592a840fb79d6f4fa1341a
Damien Lajarretie <damien@slite.com>
2022-12-16 23:35:17 +0100

    chore(tooling): enable typescript

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import { unregister } from 'utils/registerServiceWorker';
 import store from 'stores/store';
 import { Provider } from 'react-redux';
 
-// eslint-disable-next-line
 import './index.css';
 
 const rootEl = document.getElementById('root');
@@ -22,12 +21,3 @@ ReactDOM.render(
 if (typeof window.process === 'undefined') {
   unregister(); // Get rid of the service worker, we don't need it
 }
-
-// if (module.hot) {
-//   module.hot.accept('components/App', () => {
-//     // eslint-disable-next-line global-require
-//     const NextApp = require('components/App').default;
-
-//     ReactDOM.render(<NextApp />, rootEl);
-//   });
-// }
